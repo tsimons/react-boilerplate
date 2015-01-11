37221543ae8237fc0e39a52d6fbb327abd6490a6
TJ <t.simons88@gmail.com>
2015-01-10 23:05:00 -0600

    javascript gulp task functioning

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp')
   , browserify = require('gulp-browserify')
   , uglify = require('gulp-uglify')
+  , rename = require('gulp-rename')
+  , livereload = require('gulp-livereload')
 ;
 
 gulp.task('js', function () {
@@ -9,5 +11,16 @@ gulp.task('js', function () {
       extensions: [".jsx"]
     }))
     .pipe(uglify())
+    .pipe(rename('app.js'))
     .pipe(gulp.dest('build/js'))
+    .pipe(livereload())
+});
+
+gulp.task('watch', function () {
+  livereload.listen();
+  console.log('Now listening for changes...');
+  gulp.watch(['client/js/**/*.js', 'client/js/**/*.jsx'], ['js']);
+})
+
+gulp.task('default', ['watch', 'js'], function () {
 });
