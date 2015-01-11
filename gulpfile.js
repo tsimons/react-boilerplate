bbfe6a80cb373d72bd0eabe5c519593802b9b0d9
TJ <t.simons88@gmail.com>
2015-01-11 00:05:20 -0600

    added css task. Now plumbing and notifying

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,61 @@ var gulp = require('gulp')
   , browserify = require('gulp-browserify')
   , uglify = require('gulp-uglify')
   , rename = require('gulp-rename')
+  , sass = require('gulp-sass')
   , livereload = require('gulp-livereload')
+  , notify = require('gulp-notify')
+  , plumber = require('gulp-plumber')
 ;
+
+gulp.task('css', function () {
+  return gulp.src('client/sass/app.scss')
+    .pipe(sass({
+      onSuccess: notify({
+        title: 'Sass',
+        message: 'Successfully Compiled',
+        icon: 'assets/sass.png'
+      }),
+      onError: function (err) {
+        return notify().write(err);
+      }
+    }))
+    .pipe(gulp.dest('build/css'))
+    .pipe(notify({
+      title: 'Sass',
+      message: 'Successfully compiled',
+      icon: 'assets/sass.png'
+    }))
+    .pipe(livereload())
+  ;
+})
 
 gulp.task('js', function () {
   return gulp.src('client/js/index.jsx')
+    .pipe(plumber({errorHandler: notify.onError({
+      title: 'ERROR',
+      message: '<%= error.message %>',
+      icon: 'assets/js.png'
+    })}))
     .pipe(browserify({
-      extensions: [".jsx"]
+      extensions: ['.jsx']
     }))
     .pipe(uglify())
     .pipe(rename('app.js'))
     .pipe(gulp.dest('build/js'))
+    .pipe(notify({
+      title: 'SUCCESS',
+      icon: 'assets/js.png'
+    }))
     .pipe(livereload())
+  ;
 });
 
 gulp.task('watch', function () {
   livereload.listen();
   console.log('Now listening for changes...');
   gulp.watch(['client/js/**/*.js', 'client/js/**/*.jsx'], ['js']);
+  gulp.watch(['client/sass/**/*.scss'], ['css']);
 })
 
-gulp.task('default', ['watch', 'js'], function () {
+gulp.task('default', ['watch', 'js', 'css'], function () {
 });
